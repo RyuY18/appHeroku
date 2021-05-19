4cb0dd27b2ea192d3ae1d185f960bef358d63a5a
RyuY18 <nanimositakunai00@gmail.com>
2021-05-19 23:28:40 +0900

    redirect

--- list-app/appNd.js
+++ list-app/appNd.js
@@ -8,20 +8,20 @@ app.use(express.static("public"));
 app.use(express.static("js"));
 
 app.get("/", (req, res) => {
-  res.render("top.ejs");
+  res.redirect("top.ejs");
 });
 
 app.get("/index", (req, res) => {
-  res.render("index.ejs");
+  res.redirect("index.ejs");
 });
 
 app.get("/start", (req, res) => {
-  res.render("start.ejs");
+  res.redirect("start.ejs");
 });
 
 // サーバーを起動するコードを貼り付けてください
 
 app.listen(PORT, function() {
-  console.log('start saver!');
+  console.log('start sarver!');
 });
 
