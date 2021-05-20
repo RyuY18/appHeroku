70a296caedefe7c5ba5899eae1e031a6746efa4b
RyuY18 <nanimositakunai00@gmail.com>
2021-05-20 21:25:46 +0900

    Add Procfile

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const app = express();
 const fs = require('fs');
-const path = require("path");
 const ejs = require("ejs");
 const PORT = process.env.PORT || 3000;
-app.set('view engine', 'ejs');
 
 app.use(express.static("public"));
 app.use(express.static("js"));
@@ -22,7 +20,6 @@ app.get("/start", (req, res) => {
   res.render("start.ejs");
 });
 
-// サーバーを起動するコードを貼り付けてください
 
 app.listen(PORT, function() {
   console.log('start sarver!');
