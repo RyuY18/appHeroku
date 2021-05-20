3af6a5b635f9c11e6a89378a96084c61f0c64f2a
RyuY18 <nanimositakunai00@gmail.com>
2021-05-20 09:06:49 +0900

    testvi

--- list-app/index.js
+++ list-app/index.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.use(express.static("js"));
+app.use(express.static("views"))
 
 app.get("/", (req, res) => {
   res.render("/top.ejs");
